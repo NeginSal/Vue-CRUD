97b74d8cfeae65c64695f6ac3b637649a170d0cb
NeginSal <neginsaljooghi@gmail.com>
2022-06-15 16:55:48 +0430

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+home
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {  },
+  data() {
+    return {
+
+    };
+  },
+
+}
+</script>
